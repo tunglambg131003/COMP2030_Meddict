5b17c0d7a0a4de1c47b2e8e41ed51d20b473860b
Tuan Anh Nguyen <21anh.nt@vinuni.edu.vn>
2024-01-08 14:38:36 +0700

    Update sound API

--- backend/resources/words.py
+++ backend/resources/words.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Query, status, HTTPException
 from fastapi.responses import FileResponse
 import glob
 import resources
+import gtts
+from os import path
 
 router = APIRouter()
 
@@ -40,3 +42,36 @@ async def get_illustration_from_id(id: str):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="image not found")
     return file_response
 
+@router.get("/words/sound/{lang}/{id}", tags=["words"], status_code=200)
+async def get_sound_from_id(lang: str, id: int):
+    '''
+    /words/sound/{lang}/{id} API, use for searching sound from the dictionary. (Frontend)
+    ''' 
+    if lang != "en" and lang != "vn":
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="lang must be either 'en' or 'vn'")
+    query = {"id": id}
+    print(query, lang)
+    entries = resources.dictionary_collection.find(query, limit=1)
+    # for entry in entries: 
+    #     print(entry["en"])
+    # if entry.itcount() == 0:
+    # return {"id": id, "lang": lang}
+    try:
+        entry = entries[0]
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="id not found")
+    if lang == "en":
+        # check if entry[_id]_en.mp3 exists
+        file_path = f"/var/www/sounds/{entry['_id']}_en.mp3"
+        if not path.exists(file_path):
+            tts = gtts.gTTS(entry["en"], lang="en", slow=True)
+            tts.save(file_path)
+        file_response = FileResponse(file_path)
+    if lang == "vn":
+        # check if entry[_id]_vn.mp3 exists
+        file_path = f"/var/www/sounds/{entry['_id']}_vn.mp3"
+        if not path.exists(file_path):
+            tts = gtts.gTTS(entry["vn"], lang="vn", slow=True)
+            tts.save(file_path)
+        file_response = FileResponse(file_path)
+    return file_response
